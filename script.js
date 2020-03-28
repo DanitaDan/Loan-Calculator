ca03480a82bac002acda0683984e93f7bd98d8b6
Dan Danita <34411052+DanitaDan@users.noreply.github.com>
2020-03-28 04:34:58 +0000

    updated script.js, added calculation formulas

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-alert(1);
+//Listen for submit
+document.getElementById("loan-form").addEventListener("submit", calculateResults);
+
+
+//Calculate Result
+function calculateResults(e) {
+  console.log("Calculating");
+
+  // UI Variables
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("year");
+  const monthlyPayment = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calcInterest = parseFloat(interest.value) / 100 / 12;
+  const calcPayments = parseFloat(year.value) * 12;
+
+  // Compute monthly payment
+
+  const x = Math.pow(1 + calcInterest, calcPayments);
+  const monthly = (principal * x * calcInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calcPayments).toFixed(2);
+    totalInterest.value = ((monthly * calcPayments) - principal).toFixed(2);
+  } else {
+    console.log(error);
+  }
+
+
+
+
+  e.preventDefault();
+}
+
+
+
+
+
